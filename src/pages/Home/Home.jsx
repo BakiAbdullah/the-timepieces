c01183411494c174d5a11921c9fbdcc7649547ca
BakiAbdullah <shatil605@gmail.com>
2023-04-19 13:24:24 +0600

    first commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import man from '../../assets/man.jpg'
 
 const Home = () => {
   return (
-    <div>
-      
+    <div className="hero min-h-screen bg-[url('/public/banner.jpg')]">
+      <div className="hero-overlay bg-opacity-30"></div>
+      <div className="flex text-left text-neutral-content">
+        <div className="max-w-2xl">
+          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <p className="mb-5">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+        <div className="max-w-2xl">
+          <img className="w-[400px] opacity-70" src={man} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import Main from '../layouts/Main';
+import Home from '../pages/Home/Home';
 
-const routes = () => {
-  return (
-    <div>
-      
-    </div>
-  );
-};
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+    ],
+  },
+]);
 
-export default routes;
+export default router;
